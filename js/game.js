91948ccd06802ceaacac027895d9e0da09fc2edd
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-03-02 10:41:37 +0200

    initializing consts

---
+++ js/game.js
@@ -0,0 +1,15 @@
+// var audio = new Audio('./sounds/blue.mp3');
+// audio.play();
+const audioFile = ['blue','blue','blue','blue']
+
+const dataTileColors = ['green','blue','yellow','red']
+const dataTileGreen = document.querySelector('[data-title="green"]'); 
+const dataTileYellow = document.querySelector('[data-title="yellow"]'); 
+const dataTileRed = document.querySelector('[data-title="red"]'); 
+const dataTileBlue = document.querySelector('[data-title="blue"]'); 
+
+const finalLevel = 12;
+let startingLevel = 0;
+let highScore = 0;
+
+
