2e677a48d807db9a41071289ef3d5ce9bee35faf
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-03-02 14:53:35 +0200

    testing green button

--- js/game.js
+++ js/game.js
@@ -1,38 +1,50 @@
 const audioFile = ['blue','green','yellow','red','wrong','game-over','game-win']
-let audio;
 
 const dataTileColors = ['green','blue','yellow','red']
 const gameBoard = document.querySelector(".btoard")
 const startBtn = document.getElementById("play")
 const startingLevel = document.getElementById("level")
 
+//[data-tile="green"]
+
 const dataTileGreen = document.querySelector('.inactive'); 
 const dataTileYellow = document.querySelector('.inactive'); 
 const dataTileRed = document.querySelector('.inactive'); 
 const dataTileBlue = document.querySelector('.inactive'); 
 
+
+
 const finalLevel = 12;
 let level = 0;
 let highScore = 0;
-let win = false;
+let win;
+let compTurn;
+let on = false;
+let flash;
+let good;
+let interValId;
 let order = [];
 let playerOrder = [];
 
-function blueAudio(){
-    audio = new Audio(`./sounds/${audioFile[0]}.mp3`)
+function blueBtn(){
+    let audio = new Audio(`./sounds/${audioFile[0]}.mp3`)
     audio.play();
+    dataTileBlue.style.opacity = '100%'
 }
-function greenAudio(){
-    audio = new Audio(`./sounds/${audioFile[1]}.mp3`)
-    return audio.play();
+function greenBtn(){
+    let audio = new Audio(`./sounds/${audioFile[1]}.mp3`)
+    audio.play();
+    dataTileGreen.style.opacity = '100%'
 }
-function yellowAudio(){
+function yellowBtn(){
     audio = new Audio(`./sounds/${audioFile[2]}.mp3`)
-    return audio.play();
+    audio.play();
+    dataTileYellow.style.opacity = '100%'
 }
-function redAudio(){
+function redBtn(){
     audio = new Audio(`./sounds/${audioFile[3]}.mp3`)
-    return audio.play();
+    audio.play();
+    dataTileRed.style.opacity = '100%'
 }
 function wrongAudio(){
     audio = new Audio(`./sounds/${audioFile[4]}.mp3`)
@@ -47,6 +59,13 @@ function gameWinAudio(){
     return audio.play();
 }
 
+function clearColor(){
+    dataTileGreen.style.opacity = '30%';
+    dataTileBlue.style.opacity = '30%';
+    dataTileYellow.style.opacity = '30%';
+    dataTileRed.style.opacity = '30%';
+}
+
 
 startBtn.addEventListener('click', (event) => {
 
@@ -58,18 +77,56 @@ function startGame(){
     let win = false;
     highScore = 0;
     level = 1;
-    startingLevel.innerHTML = level
-    
+    startingLevel.innerHTML = level;
+    flash = 0;
+    interValId = 0;
+    good = true;
+    clearColor();
+
+    const board = document.querySelector('.board');
+    board.style.pointerEvents = 'auto'
+
     for (let i=0; i < 12; i++){
         order.push(Math.floor(Math.random() * 4) + 1);
     }
-    console.log(order)
+    compTurn = true;
 
+    interValId = setInterval(gameTurn, 800)
 }
 
-function clearColor(){
-    dataTileGreen.style.opacity = '30%';
-    dataTileBlue.style.opacity = '30%';
-    dataTileYellow.style.opacity = '30%';
-    dataTileRed.style.opacity = '30%';
+function gameTurn(){
+    on = false;
+
+    if (flash == level){
+        clearInterval(interValId);
+        compTurn = false;
+        clearColor();
+        on = true;
+    }
+
+    if (compTurn){
+        clearColor();
+        setTimeout(() => {
+            if (order[flash] == 0) blueBtn();
+            if (order[flash] == 1) greenBtn();
+            if (order[flash] == 2) yellowBtn();
+            if (order[flash] == 3) redBtn();
+            flash++;
+        }, 200)
+    }
 }
+
+
+dataTileGreen.addEventListener('click', (event) =>{
+    console.log("clicked")
+    if (on){
+        playerOrder.push(1);
+        check()
+        greenBtn()
+        if(!win){
+            setTimeout(() => {
+                clearColor()
+            }, 300)
+        }
+    }
+})
