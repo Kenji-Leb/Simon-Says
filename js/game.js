5a1f5506cd63b75da639a6493ae6621368cae894
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-03-02 13:43:23 +0200

    setting up audio function and other functions

--- js/game.js
+++ js/game.js
@@ -1,15 +1,75 @@
-// var audio = new Audio('./sounds/blue.mp3');
-// audio.play();
-const audioFile = ['blue','blue','blue','blue']
+const audioFile = ['blue','green','yellow','red','wrong','game-over','game-win']
+let audio;
 
 const dataTileColors = ['green','blue','yellow','red']
-const dataTileGreen = document.querySelector('[data-title="green"]'); 
-const dataTileYellow = document.querySelector('[data-title="yellow"]'); 
-const dataTileRed = document.querySelector('[data-title="red"]'); 
-const dataTileBlue = document.querySelector('[data-title="blue"]'); 
+const gameBoard = document.querySelector(".btoard")
+const startBtn = document.getElementById("play")
+const startingLevel = document.getElementById("level")
+
+const dataTileGreen = document.querySelector('.inactive'); 
+const dataTileYellow = document.querySelector('.inactive'); 
+const dataTileRed = document.querySelector('.inactive'); 
+const dataTileBlue = document.querySelector('.inactive'); 
 
 const finalLevel = 12;
-let startingLevel = 0;
+let level = 0;
 let highScore = 0;
+let win = false;
+let order = [];
+let playerOrder = [];
+
+function blueAudio(){
+    audio = new Audio(`./sounds/${audioFile[0]}.mp3`)
+    audio.play();
+}
+function greenAudio(){
+    audio = new Audio(`./sounds/${audioFile[1]}.mp3`)
+    return audio.play();
+}
+function yellowAudio(){
+    audio = new Audio(`./sounds/${audioFile[2]}.mp3`)
+    return audio.play();
+}
+function redAudio(){
+    audio = new Audio(`./sounds/${audioFile[3]}.mp3`)
+    return audio.play();
+}
+function wrongAudio(){
+    audio = new Audio(`./sounds/${audioFile[4]}.mp3`)
+    return audio.play();
+}
+function gameOverAudio(){
+    audio = new Audio(`./sounds/${audioFile[5]}.wav`)
+    return audio.play();
+}
+function gameWinAudio(){
+    audio = new Audio(`./sounds/${audioFile[6]}.wav`)
+    return audio.play();
+}
 
 
+startBtn.addEventListener('click', (event) => {
+
+    startGame()
+})
+
+function startGame(){
+
+    let win = false;
+    highScore = 0;
+    level = 1;
+    startingLevel.innerHTML = level
+    
+    for (let i=0; i < 12; i++){
+        order.push(Math.floor(Math.random() * 4) + 1);
+    }
+    console.log(order)
+
+}
+
+function clearColor(){
+    dataTileGreen.style.opacity = '30%';
+    dataTileBlue.style.opacity = '30%';
+    dataTileYellow.style.opacity = '30%';
+    dataTileRed.style.opacity = '30%';
+}
